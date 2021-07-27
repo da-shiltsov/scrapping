879e2cbe6142d0af7ea07aeede05df9acf9e4899
Pavel Shiltsov <pd.shiltsov@gmail.com>
2021-07-27 18:47:37 +0300

    ready-27.09

--- links.py
+++ links.py
@@ -2,36 +2,56 @@ import scrapy
 from scrapy.crawler import CrawlerProcess
 from scrapy.linkextractors import LinkExtractor
 
-result_outer_links = {}
+SITE = "https://www.bc-clinic.ru"
+SITE_DOMAIN = "bc-clinic.ru"
+IGNORED_DOMAINS = ["bc-clinic.ru", "facebook.com", "instagram.com", "vk.com", "wa.me", "google.com", "youtube.com", "medesk.ru"]
+LOG_OUTPUT = True
+REPORT_FILE = "./bcclinic-outerlinks.txt"
+
+result_outer_links = dict()
 
 class LinkSpider(scrapy.Spider):
     name = "mytestcrawler"
     visited_inner_links = set()
 
+    custom_settings = {
+        'LOG_ENABLED': False,
+    }
+
     def start_requests(self):
-        yield scrapy.Request(url = "http://asperito.ru/1.php", callback = self.parse)
+        yield scrapy.Request(url = SITE, callback = self.parse)
 
     def parse(self, response):
         global result_outer_links
-        print("# parse call: ", response.url)
 
-        inner_link_extractor = LinkExtractor(allow_domains="asperito.ru")
+        inner_link_extractor = LinkExtractor(allow_domains=SITE_DOMAIN)
         inner_links = inner_link_extractor.extract_links(response)
 
-        outer_link_extractor = LinkExtractor(deny_domains="asperito.ru")
+        outer_link_extractor = LinkExtractor(deny_domains=IGNORED_DOMAINS)
         outer_links = outer_link_extractor.extract_links(response)
 
-        result_outer_links[response.url] = outer_links
-        print("# outer links:", outer_links)
+        if len(outer_links) > 0:
+            result_outer_links[response.url] = [link.url for link in outer_links]
+            if LOG_OUTPUT:
+                print("# found in ", response.url , ": ", result_outer_links[response.url])
 
         for link in inner_links:
             if link not in self.visited_inner_links:
-                self.visited_inner_links.append(link)
-                print("# link: ", link)
-                yield response.follow(url = link, callback = self.parse)
+                self.visited_inner_links.add(link)
+                yield response.follow(url = link.url, callback = self.parse)
 
 
-print("поехали")
+print("start scanning ...")
 process = CrawlerProcess()
 process.crawl(LinkSpider)
 process.start()
+
+with open(REPORT_FILE,"w+") as fp:
+    fp.write(f"Outer links for domain {SITE}\n")
+    fp.write( "--------------------------------------------------------------------------------------------------\n\n")
+    for link, outer_links in result_outer_links.items():
+        fp.write("\n" + link + "\n")
+        for outer_link in outer_links:
+            fp.write("\t" + outer_link + "\n")
+
+print("done")
