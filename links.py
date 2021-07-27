6ca97200c603b736308ce8cef077be0aaa7f5fe4
Pavel Shiltsov <pd.shiltsov@gmail.com>
2021-07-27 16:26:02 +0300

    щл

--- links.py
+++ links.py
@@ -12,23 +12,26 @@ class LinkSpider(scrapy.Spider):
         yield scrapy.Request(url = "http://asperito.ru/1.php", callback = self.parse)
 
     def parse(self, response):
+        global result_outer_links
+        print("# parse call: ", response.url)
+
         inner_link_extractor = LinkExtractor(allow_domains="asperito.ru")
-        outer_link_extractor = LinkExtractor(deny_domains="asperito.ru")
         inner_links = inner_link_extractor.extract_links(response)
+
+        outer_link_extractor = LinkExtractor(deny_domains="asperito.ru")
         outer_links = outer_link_extractor.extract_links(response)
 
         result_outer_links[response.url] = outer_links
+        print("# outer links:", outer_links)
 
         for link in inner_links:
             if link not in self.visited_inner_links:
+                self.visited_inner_links.append(link)
+                print("# link: ", link)
+                yield response.follow(url = link, callback = self.parse)
 
-        print("------------- inner links:")
-        [print(link) for link in inner_links]
-        print("------------- outer links:")
-        [print(link) for link in outer_links]
 
-    def create_result(self, response)
-
+print("поехали")
 process = CrawlerProcess()
 process.crawl(LinkSpider)
 process.start()
